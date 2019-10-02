03849b45385f4bcdcf23e5a322e7d35de07243d8
baiyan <1105874777@qq.com>
2019-10-03 05:53:16 +0800

    从kafka读取消息并写入hdfs

--- connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
+++ connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
@@ -2,13 +2,14 @@ package com.baiyan.kafkaconnector
 
 import java.util.Properties
 
-import org.apache.flink.streaming.api.datastream.DataStream
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala.DataStream
+
 import org.apache.flink.streaming.connectors.fs.StringWriter
 import org.apache.flink.streaming.connectors.fs.bucketing.BucketingSink
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
-
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.scala._
 /*
 从kafka读取消息写入hdfs
  */
@@ -20,11 +21,12 @@ object HdfsConnector {
     // only required for Kafka 0.8
     properties.setProperty("zookeeper.connect", "localhost:2181")
     properties.setProperty("group.id", "test")
-    val unit: DataStream[String] = env.addSource(new FlinkKafkaConsumer08[String]("test", new SimpleStringSchema(), properties))
+    val unit: DataStream[String] = env.addSource(new FlinkKafkaConsumer08[String]("test1", new SimpleStringSchema(), properties))
     val counts: DataStream[(String, Int)] = unit.flatMap(x => x.split(" ")).map(x => (x, 1)).keyBy(0).sum(1)
+
     counts.print()
 
-    val date: BucketingSink[(String,Int)] = new BucketingSink("hdfs://192.168.96.181:9000/date/")
+    val date: BucketingSink[(String,Int)] = new BucketingSink("hdfs://localhost:9000/flink")
 
 
     date.setWriter(new StringWriter())
