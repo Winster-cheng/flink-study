1e58d0d63b8f2bbe97b718de863d3bfaa2835efe
baiyan <1105874777@qq.com>
2019-10-26 19:21:19 +0800

    注解

--- connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
+++ connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
@@ -11,7 +11,7 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.api.scala._
 /*
-从kafka读取消息并且写入hdfs，默认writer就是StringWriter
+从kafka读取消息并且写入hdfs
  */
 object HdfsConnector {
   def main(args: Array[String]): Unit = {
