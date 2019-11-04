0ca77a2eb45168b0f33dd74deadd73c9abaec551
baiyan <1105874777@qq.com>
2019-11-04 10:36:24 +0800

    修改hdfs说明

--- connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
+++ connector/src/main/scala/com/baiyan/kafkaconnector/HdfsConnector.scala
@@ -11,7 +11,7 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.api.scala._
 /*
-从kafka读取消息并且写入hdfs
+从kafka读取消息并且写入hdfs，核心是addSource+addSink
  */
 object HdfsConnector {
   def main(args: Array[String]): Unit = {
